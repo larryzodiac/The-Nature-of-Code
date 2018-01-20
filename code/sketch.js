e53e0bf43dfa288e653f84de3b05481a97057939
larryzodiac <09kevinsevanmachale@gmail.com>
2018-01-20 22:33:56 +0000

    I.1-2 > Random Walker // Part a.

--- code/sketch.js
+++ code/sketch.js
@@ -3,34 +3,56 @@
 // Evan MacHale - N00150552
 // Interactive Graphics Module Year 3
 // The Nature of Code by Daniel Shiffman
-// P5.js recap exercise, intro to classes + objects
+// Introduction // I.1-2 Random Walker // Part a.
+// http://natureofcode.com/book/introduction/
 // ------------------------------------------------- //
 // Class
-function Car() {
+function walker() {
   // Data
-  this.color = 'blue';
-  this.xpos = 10;
-  // Behaviours, or, functionality
+  this.x = width/2;
+  this.y = height/2;
+  // Functionality
   this.display = function() {
-    rect(this.xpos,50,100,50);
-    fill(this.color);
+    stroke(0);
+    point(this.x,this.y);
   }
-  this.drive = function() {
-    this.xpos = this.xpos + 1;
+  this.walk = function() {
+    // Number between 0 & 4 (max 3.999)
+    // 'floor' knocks off decimals
+    // Four outcomes:
+    // 0, 1, 2, or 3
+    // https://p5js.org/reference/#/p5/floor
+    var direction = floor(random(4));
+    if(direction === 0) {
+      this.x++;
+    }
+    else if (direction === 1) {
+      this.x--;
+    }
+    else if (direction === 2) {
+      this.y++;
+    }
+    else {
+      this.y--;
+    }
+    // p5 function 'constrain'
+    // Limit x position
+    // So it won't go off screen
+    // https://p5js.org/reference/#/p5/constrain
+    this.x = constrain(this.x,0,width);
+    this.y = constrain(this.y,0,height);
   }
 }
-// Declare my class object
-var myCar;
+// Declare class object
+var myWalker;
 
 function setup() {
-  createCanvas(500,500)
-  // Initialise my class object
-  myCar = new Car();
+  createCanvas(500,500);
+  //Initialise new class object
+  myWalker = new walker();
 }
-
+// Call functionality on object
 function draw() {
-  background('black');
-  // Call functionality on class object
-  myCar.display();
-  myCar.drive();
+  myWalker.display();
+  myWalker.walk();
 }
