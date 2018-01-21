21e8c0ddd2a9912fb01cabe150bc4bb3c51cf644
larryzodiac <09kevinsevanmachale@gmail.com>
2018-01-21 23:20:50 +0000

    I.3 > Probability and Non-Uniform Distributions // Part a.

--- code/sketch.js
+++ code/sketch.js
@@ -3,37 +3,62 @@
 // Evan MacHale - N00150552
 // Interactive Graphics Module Year 3
 // The Nature of Code by Daniel Shiffman
-// Introduction // I.2 Random Number Distribution
+// Introduction // I.3 Probability and Non-Uniform Distributions // Part a.
 // http://natureofcode.com/book/introduction/
 // ------------------------------------------------- //
-// Declare Array
-var myArray = [];
-// Decalre number of items to be in array
-var itemsInArray = 10;
+function Walker() {
+  // Data
+  this.x = width/2;
+  this.y = height/2;
+  // Functionality
+  this.display = function() {
+    stroke(0);
+    point(this.x,this.y);
+  }
+  this.walk = function() {
+    // Manually set probability
+    var probArray = [];
+    probArray[0] = 1;
+    probArray[1] = 1;
+    probArray[2] = 2;
+    probArray[3] = 3;
+    probArray[4] = 3;
+    // 40% chance r = 1 or 3
+    // 20% chance r = 2
+    var index = floor(random(probArray.length));
+    var r = probArray[index];
+    // More likely to be x++ & y++
+    // i.e, diagonal bottom-right
+    if (r === 1) {
+      this.x++;
+    }
+    else if (r === 2) {
+      this.x--;
+    }
+    else if (r === 3) {
+      this.y++;
+    }
+    else {
+      this.y--;
+    }
 
-function setup() {
-  createCanvas(500,500);
-  // Populate the array with number of items
-  // All w/ values of 0
-  for(var x = 0; x < itemsInArray; x++) {
-    myArray[x] = 0;
+    // for(var x = 0; x < probArray.length; x++) {
+    //     console.log(r);
+    // }
+
+    this.x = constrain(this.x,0,width);
+    this.y = constrain(this.y,0,height);
   }
 }
 
+var myWalker;
+
+function setup() {
+  createCanvas(500,500);
+  myWalker = new Walker();
+}
+
 function draw() {
-  background('black');
-  // Random index position from array
-  var index = floor(random(itemsInArray));
-  // Value of item in array @ position 'index' incremented
-  myArray[index]++;
-  // Draw rectangles
-  stroke(0);
-  strokeWeight(2);
-  fill('lime');
-  // used to calculate xPos of rects as they iterate
-  var w = width/myArray.length;
-  // Loop through array + draw rectangles for each index
-  for(var x = 0; x < myArray.length; x++) {
-    rect(x*w,height-myArray[x],w-1,myArray[x]);
-  }
+  myWalker.display();
+  myWalker.walk();
 }
