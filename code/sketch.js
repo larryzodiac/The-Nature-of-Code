bb0cd2a6cd53d875ed117c282b6b9e99820148ad
larryzodiac <09kevinsevanmachale@gmail.com>
2018-01-21 22:43:56 +0000

    I.2 > Random Number Distribution

--- code/sketch.js
+++ code/sketch.js
@@ -1,45 +1,39 @@
 // ------------------------------------------------- //
-// 20/01/2018
+// 21/01/2018
 // Evan MacHale - N00150552
 // Interactive Graphics Module Year 3
 // The Nature of Code by Daniel Shiffman
-// Introduction // I.1-2 Random Walker // Part b.
+// Introduction // I.2 Random Number Distribution
 // http://natureofcode.com/book/introduction/
 // ------------------------------------------------- //
-// Class
-function Walker() {
-  // Data
-  this.x = width/2;
-  this.y = height/2;
-  // Functionality
-  this.display = function() {
-    stroke(0);
-    point(this.x,this.y);
-  }
-  this.walk = function() {
-    // 3 outcomes: -1,0 & 1
-    xWalk = floor(random(-1,2));
-    yWalk = floor(random(-1,2));
-    // Combined we get 9 outcomes
-    // +1 xPos & +1 yPos produces diagonal
-    this.x = this.x + xWalk;
-    this.y = this.y + yWalk;
-    // Doesn't continue off screen
-    this.x = constrain(this.x,0,width);
-    this.y = constrain(this.y,0,height);
-  }
-}
-// Declare object
-var myWalker;
+// Declare Array
+var myArray = [];
+// Decalre number of items to be in array
+var itemsInArray = 10;
 
 function setup() {
   createCanvas(500,500);
-  background('lime');
-  // Initialise class objcet
-  myWalker = new Walker();
+  // Populate the array with number of items
+  // All w/ values of 0
+  for(var x = 0; x < itemsInArray; x++) {
+    myArray[x] = 0;
+  }
 }
-// Call functionality on object
+
 function draw() {
-  myWalker.display();
-  myWalker.walk();
+  background('black');
+  // Random index position from array
+  var index = floor(random(itemsInArray));
+  // Value of item in array @ position 'index' incremented
+  myArray[index]++;
+  // Draw rectangles
+  stroke(0);
+  strokeWeight(2);
+  fill('lime');
+  // used to calculate xPos of rects as they iterate
+  var w = width/myArray.length;
+  // Loop through array + draw rectangles for each index
+  for(var x = 0; x < myArray.length; x++) {
+    rect(x*w,height-myArray[x],w-1,myArray[x]);
+  }
 }
