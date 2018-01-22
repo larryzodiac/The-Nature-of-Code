ad0183389e904c3bfb3aa0cfd4b058ee36039e30
larryzodiac <09kevinsevanmachale@gmail.com>
2018-01-22 15:58:05 +0000

    I.6 > Perlin Noise // Part a.

--- code/sketch.js
+++ code/sketch.js
@@ -3,31 +3,49 @@
 // Evan MacHale - N00150552
 // Interactive Graphics Module Year 3
 // The Nature of Code by Daniel Shiffman
-// Introduction // I.4 Normal/Gaussian Distribution of Random Numbers // Part b.
+// Introduction // I.6 Perlin Noise // Part a.
+// One dimensional Noise
 // http://natureofcode.com/book/introduction/
 // ------------------------------------------------- //
+// Class
+function Walker() {
+  // Data
+  this.x = width/2;
+  this.xOff = random(1000);
+  // Functionality
+  this.display = function() {
+    noStroke();
+    fill(255,50,50,50);
+    ellipse(this.x,height/2,50,50);
+  }
+  this.walk = function() {
+    // 'noise' function always returns between 1 and 0
+    // Think of one-dimensional Perlin noise as a linear sequence of values over time
+    // In this case 'xOff' is our time
+    // 'map' used to get our current value (xOff) within a range (1,0)
+    // Then return a new value from a new given range
+    // 'new value = map(value, current min, current max, new min, new max)'
+    this.x = map(noise(this.xOff),0,1,0,width);
+    // console.log(this.x);
+    // 'xOff' moves us forward in time
+    // 'a=a+b' === 'a+=b'
+    this.xOff += 0.0020;
+  }
+}
+// Declare object
+var myWalker;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('black');
+  // Initialise class object
+  myWalker = new Walker();
 }
 
 function draw() {
-  // Normal/Gaussian distribution
-  var xloc = randomGaussian();
-  var yloc = randomGaussian();
-  // Standard deviation
-  var xsd = 100;
-  var ysd = 90;
-  // Mean
-  var xMean = windowWidth/2;
-  var yMean = windowHeight/2;
-  // Scale by sd & mean
-  // Distribution of values clustered around an average
-  // -> Gaussian or Normal distribution
-  xloc = (xloc * xsd) + xMean;
-  yloc = (yloc * ysd) + yMean;
-  var rColor = 250;
-  fill(rColor, 50);
-  noStroke();
-  ellipse(xloc,yloc,5,5);
+  // New 'rect' drawn every frame with a slight opacity
+  // Creates trailing effect when ellipse moves
+  fill(0,15);
+  rect(0,0,width,height);
+  myWalker.display();
+  myWalker.walk();
 }
