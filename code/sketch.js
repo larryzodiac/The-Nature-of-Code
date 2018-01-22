756bd6b884798fed9eed782a4c3a8bdc796c850d
larryzodiac <09kevinsevanmachale@gmail.com>
2018-01-22 15:15:51 +0000

    I.4 > Normal/Gaussian Distribution of Random Numbers // Part b.

--- code/sketch.js
+++ code/sketch.js
@@ -1,9 +1,9 @@
 // ------------------------------------------------- //
-// 21/01/2018
+// 22/01/2018
 // Evan MacHale - N00150552
 // Interactive Graphics Module Year 3
 // The Nature of Code by Daniel Shiffman
-// Introduction // I.4 A Normal/Gaussian Distribution of Random Numbers // Part a.
+// Introduction // I.4 Normal/Gaussian Distribution of Random Numbers // Part b.
 // http://natureofcode.com/book/introduction/
 // ------------------------------------------------- //
 function setup() {
@@ -14,14 +14,20 @@ function setup() {
 function draw() {
   // Normal/Gaussian distribution
   var xloc = randomGaussian();
+  var yloc = randomGaussian();
   // Standard deviation
-  var sd = 150;
-  var mean = width/2;
+  var xsd = 100;
+  var ysd = 90;
+  // Mean
+  var xMean = windowWidth/2;
+  var yMean = windowHeight/2;
   // Scale by sd & mean
   // Distribution of values clustered around an average
   // -> Gaussian or Normal distribution
-  xloc = (xloc * sd) + mean;
-  fill('rgba(0,255,0, 0.25)');
+  xloc = (xloc * xsd) + xMean;
+  yloc = (yloc * ysd) + yMean;
+  var rColor = 250;
+  fill(rColor, 50);
   noStroke();
-  ellipse(xloc,height/2,20,20);
+  ellipse(xloc,yloc,5,5);
 }
