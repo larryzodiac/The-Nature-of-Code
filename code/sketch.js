05ae08713b1aab8f7e6bbb9ff8012849ad61ebba
larryzodiac <09kevinsevanmachale@gmail.com>
2018-01-22 00:25:11 +0000

    I.4 A Normal/Gaussian Distribution of Random Numbers // Part a.

--- code/sketch.js
+++ code/sketch.js
@@ -3,50 +3,25 @@
 // Evan MacHale - N00150552
 // Interactive Graphics Module Year 3
 // The Nature of Code by Daniel Shiffman
-// Introduction // I.3 Probability and Non-Uniform Distributions // Part b.
+// Introduction // I.4 A Normal/Gaussian Distribution of Random Numbers // Part a.
 // http://natureofcode.com/book/introduction/
 // ------------------------------------------------- //
-function Walker() {
-  // Data
-  this.x = width/2;
-  this.y = height/2;
-  // Functionality
-  this.display = function() {
-    stroke(0);
-    point(this.x,this.y);
-  }
-  this.walk = function() {
-    var r = random(1);
-    // ~40% chance of moving right
-    if (r < 0.4) {
-      this.x++;
-    }
-    // ~20%
-    else if (r < 0.6) {
-      this.x--;
-    }
-    else if (r < 0.8) {
-      this.y++;
-    }
-    else {
-      this.y--;
-    }
-    // So point won't go off canvas
-    this.x = constrain(this.x,0,width);
-    this.y = constrain(this.y,0,height);
-  }
-}
-// Declare object
-var myWalker;
-
 function setup() {
-  createCanvas(500,500);
-  // Initialise class object
-  myWalker = new Walker();
+  createCanvas(windowWidth, windowHeight);
+  background('black');
 }
 
 function draw() {
-  // Call functionality on object
-  myWalker.display();
-  myWalker.walk();
+  // Normal/Gaussian distribution
+  var xloc = randomGaussian();
+  // Standard deviation
+  var sd = 150;
+  var mean = width/2;
+  // Scale by sd & mean
+  // Distribution of values clustered around an average
+  // -> Gaussian or Normal distribution
+  xloc = (xloc * sd) + mean;
+  fill('rgba(0,255,0, 0.25)');
+  noStroke();
+  ellipse(xloc,height/2,20,20);
 }
